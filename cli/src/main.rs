76487a5a2c77bb7325229c728f5c415700051be5
letitride <ichikawa.fumiya@gmail.com>
2020-11-16 11:31:23 +0900

    コマンドライン オプションとサブコマンドの設定

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,55 @@
+use clap::{Arg, App, AppSettings, SubCommand, _clap_count_exprs, arg_enum};
+
+arg_enum! {
+    #[derive(Debug)]
+    enum Format {
+        Csv,
+        Json,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = App::new(env!("CARGO_PKG_NAME"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .arg(
+            Arg::with_name("SERVER")
+                .short("s")
+                .long("server")
+                .value_name("URL")
+                .help("server url")
+                .takes_value(true)
+        )
+        .subcommand(SubCommand::with_name("post").about("post logs. taking input from stdin"))
+        .subcommand(
+            SubCommand::with_name("get").about("get logs").arg(
+                Arg::with_name("FORMAT")
+                    .help("log format")
+                    .short("f")
+                    .long("format")
+                    .takes_value(true)
+                    .possible_values(&Format::variants())
+                    .case_insensitive(true)
+            )
+        );
+    
+    let matches = opts.get_matches();
+    let server = matches.value_of("SERVER").unwrap_or("localhost:3000");
+    match matches.subcommand() {
+        ("get", sub_match) => {
+            println!("get: {:?}", sub_match);
+            let format = sub_match
+                .and_then(|m| m.value_of("FORMAT"))
+                .map(|m| m.parse().unwrap())
+                .unwrap();
+            match format {
+                Format::Csv => unimplemented!(),
+                Format::Json => unimplemented!(),
+            }
+        },
+        ("post", sub_match) => println!("post: {:?}", sub_match),
+        _ => unreachable!(),
+    }
 }
